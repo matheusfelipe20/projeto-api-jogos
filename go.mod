cae80fc4ca3af44afaf479203987221fc2615cd6
Eduardo Lima <edwdilim@hotmail.com>
2022-07-21 12:42:42 -0300

    router created

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/matheusfelipe20/projeto-api-jogos
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/matheusfelipe20/projeto-api-jogos/src/router"
+)
+
 func main() {
+
+	fmt.Println("Rodando")
+	r := router.GerarRota()
+	log.Fatal(http.ListenAndServe(":8080",r))
 
 }
