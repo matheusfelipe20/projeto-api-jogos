1a865e0e62019fc5f1e1cff44d423c207a68537f
Matheus Felipe <felipe_2012matheus@hotmail.com>
2022-07-21 16:39:04 -0300

    Create func vendas

---
+++ src/router/models/vendas.go
@@ -0,0 +1,15 @@
+package models
+
+import "net/http"
+
+func GetCPF(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetCampeonatos(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetJogos(w http.ResponseWriter, r *http.Request) {
+
+}
