3e528d4547a0dc3a3ccb47d30875e2d20323c295
Eduardo Lima <edwdilim@hotmail.com>
2022-07-28 20:51:14 -0300

    refatorando config e db

--- src/configs/config.go
+++ src/configs/config.go
@@ -1,72 +1,37 @@
 package configs
 
-import(
-	"github.com/spf13/viper"
+import (
+	"log"
+	"os"
+	"strconv"
+	"fmt"
+
+	"github.com/joho/godotenv"
 )
 
-var cfg *config
+var(
+	StringConexaoBanco = ""
+	Porta = 0
+)
 
-type config struct{
-	API APIConfig
-	DB DBConfig
-}
+// Carregar inicializa as variáveis de ambiente
+func Carregar(){
+	var err error
 
-// Porta da api
-type APIConfig struct{
-	Port string
-}
-
-// Configurações do banco de dados
-type DBConfig struct{
-	Host string
-	Port string
-	User string
-	Password string
-	Database string
-}
-
-// incialização das configurações da api
-func init(){
-	viper.SetDefault("api.port", ":5000")
-	viper.SetDefault("database.host", "localhost")
-	viper.SetDefault("database.port", "5432")
-}
-
-// Load carrega as configurações da api a partir do arquivo toml
-func Load() error{
-	viper.SetConfigName("config")
-	viper.SetConfigType("toml")
-	viper.AddConfigPath(".")
-
-	err := viper.ReadInConfig()
-	if err != nil{
-		if _, ok := err.(viper.ConfigFileNotFoundError); !ok{
-			return err
-		}
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
 	}
 
-	cfg = new(config)
-
-	cfg.API = APIConfig{
-		Port: viper.GetString("api.port"),
-	}
-	cfg.DB = DBConfig{
-		Host: viper.GetString("database.host"),
-		Port: viper.GetString("database.port"),
-		User: viper.GetString("database.user"),
-		Password: viper.GetString("database.password"),
-		Database: viper.GetString("database.name"),
+	Porta, err = strconv.Atoi(os.Getenv("API_PORT"))
+	if err != nil {
+		Porta = 4000
 	}
 
-	return nil
-}
-
-// GetDB retorna as configurações do banco de dados
-func GetDB() DBConfig{
-	return cfg.DB
-}
-
-// GetPortaServidor retorna a porta do servidor
-func GetPortaServidor() string{
-	return cfg.API.Port
+	StringConexaoBanco = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_NAME"),
+	)
 }
--- src/db/conexao.go
+++ src/db/conexao.go
@@ -2,26 +2,23 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
-
 	"github.com/matheusfelipe20/projeto-api-jogos/src/configs"
+	
 	_ "github.com/lib/pq"	// driver de conexão do postgres
 )
 
 // Conectar abre a conexão com o postgres
 func Conectar() (*sql.DB, error){
 
-	conf := configs.GetDB()
-
-	stringConexao := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", conf.Host, conf.Port, conf.User, conf.Password, conf.Database)
-
-	conn, err := sql.Open("postgres", stringConexao)
+	db, err := sql.Open("postgres", configs.StringConexaoBanco)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	err = conn.Ping()
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
 
-	return conn, err
+	return db, nil
 
 }
