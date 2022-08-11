384a525de59b4a620daa45d47c5f93bc3234d6e3
Jonas Victor <jonas.victor@dcx.ufpb.br>
2022-08-10 23:21:33 -0300

    Controller e Repositorio de usuario

--- src/controllers/usuarioController.go
+++ src/controllers/usuarioController.go
@@ -1,21 +1,64 @@
 package controllers
 
-import "github.com/matheusfelipe20/projeto-api-jogos/src/models"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
 
-var Usuarios []models.Usuario = []models.Usuario{
-	{
-		Cpf:        56722233344,
-		Nome:       "jonas",
-		Nascimento: "01/01/2022",
-	},
-	{
-		Cpf:        55512377788,
-		Nome:       "felipe",
-		Nascimento: "02/02/2022",
-	},
-	{
-		Cpf:        99988823466,
-		Nome:       "eduardo",
-		Nascimento: "03/03/2022",
-	},
+	"github.com/gorilla/mux"
+	"github.com/matheusfelipe20/projeto-api-jogos/src/db"
+	"github.com/matheusfelipe20/projeto-api-jogos/src/models"
+	"github.com/matheusfelipe20/projeto-api-jogos/src/repositories"
+)
+
+func CadastrarUsuario(w http.ResponseWriter, r *http.Request) {
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var usr models.Usuario
+	if err := json.Unmarshal(requestBody, &usr); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := db.Conectar()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	repositorio := repositories.NovoRepositorioDeUsuarios(db)
+	lastID, err := repositorio.AdicionarUsuario(usr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(fmt.Sprintf("Usuário criado com sucesso. ID: %d", lastID)))
+}
+
+func ListarUsuario(w http.ResponseWriter, r *http.Request) {
+	parametro := mux.Vars(r)
+	usuarioCPF, err := strconv.Atoi(parametro["cpf"])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := db.Conectar()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	repositorio := repositories.NovoRepositorioDeUsuarios(db)
+	usr, err := repositorio.BuscarUsuarioByCpf(usuarioCPF)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(usr)
+
 }
