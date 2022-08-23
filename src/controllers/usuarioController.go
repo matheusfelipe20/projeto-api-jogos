842d835c5ff973d1c809f429321194d125bf4dd5
Matheus Felipe <felipe_2012matheus@hotmail.com>
2022-08-22 23:35:53 -0300

    Update Validator

--- src/controllers/usuarioController.go
+++ src/controllers/usuarioController.go
@@ -10,6 +10,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/matheusfelipe20/projeto-api-jogos/src/db"
 	"github.com/matheusfelipe20/projeto-api-jogos/src/models"
+	"github.com/matheusfelipe20/projeto-api-jogos/src/models/validators"
 	"github.com/matheusfelipe20/projeto-api-jogos/src/repositories"
 	"github.com/matheusfelipe20/projeto-api-jogos/src/respostas"
 )
@@ -37,7 +38,12 @@ func CadastrarUsuario(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	// Insere o jogo no banco de dados
+	//Verificar se o usuario é maior de idade
+	if verificarNascimento := validators.ValidadeDataNascimento(usr.Nascimento); !verificarNascimento {
+		http.Error(w, "Erro!! usuario menor de idade", http.StatusInternalServerError)
+		return
+	}
+
 	repositorio := repositories.NovoRepositorioDeUsuarios(db)
 	usr.ID, err = repositorio.AdicionarUsuario(usr)
 	if err != nil {
